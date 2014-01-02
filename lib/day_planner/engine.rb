0ee72855e11d3d3a3553dcd7fe969402df40820b
Damon Siefert <siefert@gmail.com>
2014-01-01 23:01:24 -0500

    will this make it run

--- lib/day_planner/engine.rb
+++ lib/day_planner/engine.rb
@@ -4,10 +4,10 @@ module DayPlanner
 	class Railtie < Rails::Engine
 		isolate_namespace DayPlanner
 
-		unless $0 =~ /rake/ || defined?(::Rails::Generators) || defined?(::Bundler)# || defined?(::Rails::Console) 
+		unless $0 =~ /rake/ || defined?(::Rails::Generators) || defined?(::Bundler) || defined?(::Rails::Console) 
 			initializer "day_planner.activate", after: :finisher_hook do
 				DayPlanner.clear_tasks
-				require File.expand_path('config/scheduled_tasks')
+				require Rails.root.join('config', 'scheduled_tasks')
 				DayPlanner.activate
 			end
 		end
