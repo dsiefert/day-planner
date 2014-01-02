5e3e7fdf0136290c7cca040cee8e1663f5d1c8a2
Damon Siefert <siefert@gmail.com>
2014-01-01 23:32:46 -0500

    damn i thought i'd solved this problem

--- lib/day_planner/engine.rb
+++ lib/day_planner/engine.rb
@@ -5,9 +5,9 @@ module DayPlanner
 	class Railtie < Rails::Engine
 		isolate_namespace DayPlanner
 
-		unless $0 =~ /rake/ || defined?(::Rails::Generators) || defined?(::Bundler)# || defined?(::Rails::Console) 
+		unless $0 =~ /rake/ || defined?(::Rails::Console) 
 			initializer "day_planner.activate", after: :finisher_hook do
-				puts "dayplannery stuff ignore"
+				puts "DayPlanner starting up"
 				DayPlanner.clear_tasks
 				require Rails.root.join('config', 'scheduled_tasks')
 				DayPlanner.activate
