bf43bc33173bfdcd7e30cc4c1c7d3b012705712b
Damon Siefert <siefert@gmail.com>
2014-01-02 00:29:10 -0500

    eh a production check is better than nada

--- lib/day_planner/engine.rb
+++ lib/day_planner/engine.rb
@@ -5,7 +5,7 @@ module DayPlanner
 	class Railtie < Rails::Engine
 		isolate_namespace DayPlanner
 
-		unless $0 =~ /rake/ || defined?(::Rails::Console) 
+		unless $0 =~ /rake/ || defined?(::Rails::Console) || !defined?(Rails.env) || !Rails.env.production?
 			initializer "day_planner.activate", after: :finisher_hook do
 				puts "DayPlanner starting up"
 				DayPlanner.clear_tasks
--- lib/day_planner/version.rb
+++ lib/day_planner/version.rb
@@ -2,7 +2,7 @@ module DayPlanner
 	MAJOR = 0
 	MINOR = 1
 	TINY  = 0
-	PRE   = "pre4"
+	PRE   = "pre5"
 	BUILD = nil
 
   VERSION = [MAJOR, MINOR, TINY, PRE, BUILD].compact.join(".")
