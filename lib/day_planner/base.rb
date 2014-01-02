5334e8a69d8b1c8c48974d0eae96cfaada12cb5b
Damon Siefert <siefert@gmail.com>
2014-01-02 02:04:04 -0500

    will this make you happy sqlite

--- lib/day_planner/base.rb
+++ lib/day_planner/base.rb
@@ -33,7 +33,7 @@ module DayPlanner
 
 		def clear_tasks
 			@@tasks = []
-			ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{DayPlanner::Task.table_name}")
+			ActiveRecord::Base.connection.execute("DELETE FROM #{DayPlanner::Task.table_name}")
 		end
 
 		def schedule(options, &block)
