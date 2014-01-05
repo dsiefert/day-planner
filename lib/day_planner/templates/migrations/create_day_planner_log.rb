a6f354eebe858dc9ea5c3bc379f5167f68f30959
Damon Siefert <siefert@gmail.com>
2014-01-05 18:47:41 -0500

    why did i have drop_table in an UP migration????

--- lib/day_planner/templates/migrations/create_day_planner_log.rb
+++ lib/day_planner/templates/migrations/create_day_planner_log.rb
@@ -1,7 +1,5 @@
 class CreateDayPlannerLog < ActiveRecord::Migration
 	def up
-		drop_table :day_planner_log
-
 		create_table :day_planner_log do |t|
 			t.string   :name
 			t.integer  :interval
--- lib/day_planner/version.rb
+++ lib/day_planner/version.rb
@@ -2,7 +2,7 @@ module DayPlanner
 	MAJOR = 0
 	MINOR = 1
 	TINY  = 0
-	PRE   = "pre26"
+	PRE   = "pre27"
 	BUILD = nil
 
   VERSION = [MAJOR, MINOR, TINY, PRE, BUILD].compact.join(".")
